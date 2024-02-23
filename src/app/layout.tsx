1048f667bbb122e7e732cc86cfcaab53a755421d
Redish101 <i@redish101.top>
2024-02-23 16:12:11 +0800

    fix: Minified React error #418

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import type { Metadata } from "next";
 import config from "@/../site.config";
 
 import { Container, Content, LeftSidebar, RightSidebar } from "@/components/Layout";
-import '@retalkgo/client/retalk.css'
 
 import { Analytics } from '@vercel/analytics/react';
 import Script from "next/script";
@@ -22,9 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN">
-      <head>
-        <Script src="https://jsd.onmicrosoft.cn/npm/@retalkgo/client/dist/retalk.umd.js" />
-      </head>
       <body>
         <NavBar />
         <Container>
