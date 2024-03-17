f40eb10e8a627e3a5f7618db62c9e7a6526610e0
Redish101 <i@redish101.top>
2024-03-17 15:43:46 +0800

    fix: 修复Clarity不生效的问题

--- src/components/Clarity.tsx
+++ src/components/Clarity.tsx
@@ -1,21 +1,23 @@
 'use client'
 
-import Script from "next/script";
+import Script from "next/script"
 
-export default function Clarity() {
-  return (
-    <Script
-      id="clarity"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "lhz6gsilg2");
-          `,
-      }}
-    />
-  );
+const Clarity = () => {
+    return (
+        <Script
+            id="microsoft-clarity-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "lhz6gsilg2");
+                `,
+            }}
+        />
+    )
 }
+
+export default Clarity
